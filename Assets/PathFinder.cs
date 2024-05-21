a5119a77cd466f0f1ff1ff4af71ff17d71d0990e
leeseul0919 <108906872+leeseul0919@users.noreply.github.com>
2024-05-21 17:56:46 +0900

    Add files via upload

---
+++ Assets/PathFinder.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PathFinder : MonoBehaviour
+{
+    public GameObject target;
+    public NavMeshAgent agent;
+    LineRenderer lr;
+
+    void Start()
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+
+        lr = this.GetComponent<LineRenderer>();
+        lr.startWidth = lr.endWidth = 0.1f;
+        lr.material.color = Color.blue;
+        lr.enabled = false;
+    }
+
+    public void makePath()
+    {
+        lr.enabled = true;
+        StartCoroutine(makePathCoroutine());
+    }
+
+    IEnumerator makePathCoroutine()
+    {
+        agent.SetDestination(target.transform.position);
+        lr.SetPosition(0, this.transform.position);
+
+        yield return new WaitForSeconds(1f);
+
+        drawPath();
+    }
+
+    void drawPath()
+    {
+        int length = agent.path.corners.Length;
+
+        lr.positionCount = length;
+        for (int i = 1; i < length; i++)
+            lr.SetPosition(i, agent.path.corners[i]);
+    }
+}
